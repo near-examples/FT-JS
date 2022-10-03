d721928a8c20c335d5c7195a92f451095d205a1c
Dennis <10233439+idea404@users.noreply.github.com>
2022-10-03 14:57:42 +0000

    second working test

--- tests/main.ava.js
+++ tests/main.ava.js
@@ -3,47 +3,53 @@ import test from "ava";
 
 test.beforeEach(async (t) => {
   const worker = await Worker.init();
+  
+  const totalSupply = 1000;
+  const yoctoAccountStorage = "330";
 
   const root = worker.rootAccount;
-  const totalSupply = 1000;
-  const contract = await root.devDeploy(
-    "./build/contract.wasm", 
-    { 
-        initialBalance: NEAR.parse("300 N").toJSON(),
-        method: "init",
-        args: {
-            total_supply: totalSupply.toString(),
-        }
-    }
-);
+  const contract = await root.devDeploy("./build/contract.wasm", {
+    initialBalance: NEAR.parse("300 N").toJSON(),
+    method: "init",
+    args: {
+      total_supply: totalSupply.toString(),
+    },
+  });
   const alice = await root.createSubAccount("alice", { initialBalance: NEAR.parse("10 N").toJSON() });
 
   t.context.worker = worker;
   t.context.accounts = { root, contract, alice };
-  t.context.variables = { totalSupply };
+  t.context.variables = { totalSupply, yoctoAccountStorage };
 });
 
 test.afterEach.always(async (t) => {
-    await t.context.worker.tearDown().catch(error => {
-        console.log('Failed to tear down the worker:', error);
-      });
+  await t.context.worker.tearDown().catch((error) => {
+    console.log("Failed to tear down the worker:", error);
+  });
 });
 
-test.only("should register account and pay for storage", async (t) => {
-    const { contract, alice } = t.context.accounts;
-    const result = await alice.call(contract, "storage_deposit", { account_id: alice.accountId }, { attachedDeposit: NEAR.parse("1 N").toJSON() });
-    const expected = {
-        message: `Account ${alice.accountId} registered with storage deposit of 330`,
-    };
-    t.deepEqual(result, expected);
+test("should register account and pay for storage", async (t) => {
+  const { contract, alice } = t.context.accounts;
+  const { yoctoAccountStorage } = t.context.variables;
+  const result = await alice.call(contract, "storage_deposit", { account_id: alice.accountId }, { attachedDeposit: NEAR.parse("1 N").toJSON() });
+  const aliceAfterBalance = await alice.balance();
+  const expected = {
+    message: `Account ${alice.accountId} registered with storage deposit of ${yoctoAccountStorage}`,
+  };
+  t.deepEqual(result, expected);
+  t.true(aliceAfterBalance.total > NEAR.parse("9 N").toJSON(), "alice should have received a refund");
 });
 
-test.skip("should return message when account is already registered and not refund when no deposit is attached", async (t) => {
-    const { contract, alice } = t.context.accounts;
-    const { totalSupply } = t.context.variables;
-    const result = await alice.call(contract, "storage_deposit", { account_id: alice.accountId }, { attachedDeposit: NEAR.parse("10 N").toJSON() });
-    t.is(result.message, "Account is already registered");
-    t.is(await contract.view("ft_total_supply"), totalSupply.toString());
+test("should return message when account is already registered and not refund when no deposit is attached", async (t) => {
+  const { contract, alice } = t.context.accounts;
+  const { yoctoAccountStorage } = t.context.variables;
+  const result = await alice.call(contract, "storage_deposit", { account_id: alice.accountId }, { attachedDeposit: NEAR.parse("1 N").toJSON() });
+  const expected = {
+    message: `Account ${alice.accountId} registered with storage deposit of ${yoctoAccountStorage}`,
+  };
+  t.deepEqual(result, expected);
+  const result2 = await alice.call(contract, "storage_deposit", { account_id: alice.accountId }, { attachedDeposit: NEAR.parse("1 N").toJSON() });
+  t.is(result2.message, "Account is already registered, deposit refunded to predecessor");
 });
 
 test.todo("should return message and refund predecessor caller when trying to pay for storage for an account that is already registered");
